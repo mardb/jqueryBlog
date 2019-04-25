75767a38021041403ac21f7bd7e2b66481d1e8a1
Marlene Guzman <marleneguzmanla@gmail.com>
2019-04-25 11:58:59 -0700

    Getting Values from Form Fields

--- js/app.js
+++ js/app.js
@@ -1,13 +1,24 @@
-$("#flashMessage")
-.hide()
-.slideDown(1000)
-.delay(3000)
-.slideUp();
 
-const title = "My First Blog Post";
-const content = "this is my <strong>first</strong> post!";
+
+// //blog content
+// const title = "My First Blog Post";
+// const content = "this is my <strong>first</strong> post!";
+
+
+//Getting Values from Form Fields
+$("#flashMessage").hide();
+
+$('#previewButton').click(function(){
+  const title = $("#blogTitleInput").val();
+  const content = $("#blogContentInput").val();
 
 $("#blogTitlePreview").text(title);
 $("#blogContentPreview").html(content);
 
+//animate banner 
+$("#flashMessage")
+.slideDown(1000)
+.delay(3000)
+.slideUp();
+});
 
